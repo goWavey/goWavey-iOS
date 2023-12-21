cd6066f87919811902de53c6878abe31438e6b54
matnikola <137178809+matnikola@users.noreply.github.com>
2023-12-21 19:54:23 +0100

    Initial commit

---
+++ GoWavey/Sources/GoWavey/RewardSDK.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+final class RewardSDK {
+    
+    let authToken: String
+
+    init(authToken: String) {
+        self.authToken = authToken
+    }
+}
