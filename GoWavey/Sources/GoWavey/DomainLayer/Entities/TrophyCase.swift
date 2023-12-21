cda2371aaadb9c910a18d5a7ccd80fa7a00dab45
matnikola <137178809+matnikola@users.noreply.github.com>
2023-12-21 20:06:50 +0100

    Creates Trophy Case entity

---
+++ GoWavey/Sources/GoWavey/DomainLayer/Entities/TrophyCase.swift
@@ -0,0 +1,17 @@
+//
+//  TrophyCase.swift
+//  
+//
+//  Created by Nikola Matijevic on 21.12.23..
+//
+
+import Foundation
+
+struct TrophyCase {
+    let userId: String
+    var trophies: [Badge]
+    var totalPoints: Int
+    var level: Int
+    var nextLevelThreshold: Int
+    var lastUpdated: Date
+}
