1a8036a044a76e84834263bd4380c083009b3de9
matnikola <137178809+matnikola@users.noreply.github.com>
2023-12-21 20:06:09 +0100

    Creates Reward entity

---
+++ GoWavey/Sources/GoWavey/DomainLayer/Entities/Reward.swift
@@ -0,0 +1,17 @@
+//
+//  Reward.swift
+//
+//
+//  Created by Nikola Matijevic on 21.12.23..
+//
+
+import Foundation
+
+struct Reward {
+    let id: String
+    let name: String
+    let description: String
+    let iconUrl: String
+    let value: Int
+    let type: String
+}
