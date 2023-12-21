9c225e0841a230abec9bcc81f5c77b0fd31e57d2
matnikola <137178809+matnikola@users.noreply.github.com>
2023-12-21 20:04:52 +0100

    Creates Badge entity

---
+++ GoWavey/Sources/GoWavey/DomainLayer/Entities/Badge.swift
@@ -0,0 +1,17 @@
+//
+//  Badge.swift
+//
+//
+//  Created by Nikola Matijevic on 21.12.23..
+//
+
+import Foundation
+
+struct Badge: Identifiable, Hashable {
+
+    let id: String
+    let name: String
+    let description: String
+    let iconUrl: String
+    let type: String
+}
