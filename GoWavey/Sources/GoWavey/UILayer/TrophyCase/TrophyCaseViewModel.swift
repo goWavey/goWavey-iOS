6effafc5f0c76e905f02cae87f389c689769af8f
matnikola <137178809+matnikola@users.noreply.github.com>
2024-01-20 16:45:47 +0100

    Implements  TrophyCaseVM

---
+++ GoWavey/Sources/GoWavey/UILayer/TrophyCase/TrophyCaseViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  TrophyCaseViewModel.swift
+//
+//
+//  Created by Nikola Matijevic on 20.1.24..
+//
+
+import Foundation
+import Combine
+
+protocol TrophyCaseVMDependencies {
+    var getTrophyCaseUseCase: GetTrophyCaseUseCase { get }
+}
+
+extension TrophyCaseView {
+
+    final class ViewModel: ObservableObject {
+
+        @Published var isLoading = false
+        @Published var hasFailed = false
+        @Published var trophyCase: TrophyCase?
+        private var subscriber: AnyCancellable?
+
+        let dependencies: TrophyCaseVMDependencies
+
+        init(dependencies: TrophyCaseVMDependencies) {
+
+            self.dependencies = dependencies
+        }
+
+        @MainActor
+        func getTrophyCase(id: String) async {
+
+            isLoading = true
+
+            subscriber = await dependencies.getTrophyCaseUseCase.getTrophyCase(id: id)
+                .sink { completion in
+
+                    if case .failure(let error) = completion {
+
+                        self.hasFailed = true
+                    }
+                } receiveValue: { [weak self] trophyCase in
+
+                    self?.trophyCase = trophyCase
+                    self?.isLoading = false
+                }
+        }
+    }
+}
