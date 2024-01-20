21ef3f50df326dcdfe55222b69523f89b3285964
matnikola <137178809+matnikola@users.noreply.github.com>
2024-01-20 17:37:28 +0100

    Adds toast and SFSafeSymbols

--- GoWavey/Package.swift
+++ GoWavey/Package.swift
@@ -12,11 +12,17 @@ let package = Package(
             name: "GoWaveySDK",
             targets: ["GoWaveySDK"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "4.0.0"))
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GoWaveySDK"),
+            name: "GoWaveySDK",
+        dependencies: [
+            "SFSafeSymbols"]
+        ),
         .testTarget(
             name: "GoWaveySDKTests",
             dependencies: ["GoWaveySDK"]),
