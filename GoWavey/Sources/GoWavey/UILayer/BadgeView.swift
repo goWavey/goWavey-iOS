8a89a71db80bca8b8559999c164c792c63eb6cdf
matnikola <137178809+matnikola@users.noreply.github.com>
2024-01-24 19:59:37 +0100

    Updates Badge View to support multiple badge presentation

--- GoWavey/Sources/GoWavey/UILayer/BadgeView.swift
+++ GoWavey/Sources/GoWavey/UILayer/BadgeView.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Nikola Matijevic on 26.12.23..
 //
@@ -9,38 +9,36 @@ import SwiftUI
 
 struct BadgeView: View {
 
-    let badge: Badge
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    let badges: [Badge]
 
-    
+
     var body: some View {
         ZStack {
+
             Color.black.opacity(0.6).edgesIgnoringSafeArea(.all)
 
             VStack {
+                if badges.count == 1 {
+                    viewForBadge(badges.first!)
+                } else {
 
-                Spacer()
-
-                Text("Received a new \(badge.name) Badge!")
-                    .font(.headline)
-                    .foregroundColor(.white)
-
-                AsyncImage(url: URL(string: badge.iconUrl)) { image in
-                    image.resizable()
-                         .aspectRatio(contentMode: .fit)
-                         .frame(width: 200, height: 200)
-                } placeholder: {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    TabView {
+                        ForEach(badges) { badge in
+                            viewForBadge(badge)
+                        }
+                    }
+                    .tabViewStyle(.page)
                 }
-                .padding()
-
-                Text(badge.description)
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
 
                 Spacer()
 
-                Button(action: {}) {
+
+                Button(action: {
+                    withAnimation {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }) {
                     Image(systemName: "xmark.circle.fill")
                         .font(.largeTitle)
                         .foregroundColor(.white)
@@ -48,10 +46,41 @@ struct BadgeView: View {
             }
         }
     }
+
+
+    @ViewBuilder
+    func viewForBadge(_ badge: Badge) -> some View {
+
+
+        VStack {
+
+            Spacer()
+
+            Text("Received a new \(badge.name) Badge!")
+                .font(.headline)
+                .foregroundColor(.white)
+
+            AsyncImage(url: URL(string: badge.iconUrl)) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+            } placeholder: {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            }
+            .padding()
+
+            Text(badge.description)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+
+            Spacer()
+        }
+    }
 }
 
 struct BadgeView_Previews: PreviewProvider {
     static var previews: some View {
-        BadgeView(badge: TrophyCase.mock.trophies[0])
+        BadgeView(badges: [TrophyCase.mock.trophies[0]])
     }
 }
