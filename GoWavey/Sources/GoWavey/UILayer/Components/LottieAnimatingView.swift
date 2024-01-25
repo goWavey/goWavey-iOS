0ef765d4665a633982c67f77d058732ba5b8c563
matnikola <137178809+matnikola@users.noreply.github.com>
2024-01-25 15:53:32 +0100

    Adds LottieAnimatingView and lottie animations

---
+++ GoWavey/Sources/GoWavey/UILayer/Components/LottieAnimatingView.swift
@@ -0,0 +1,51 @@
+//
+//  LottieAnimatingView.swift
+//
+//
+//  Created by Nikola Matijevic on 25.1.24..
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import Lottie
+
+struct LottieAnimatingView: UIViewRepresentable {
+    enum Lottie: String {
+        case animation1
+        case animation2
+        case animation3
+        case animation4
+        case animation5
+        case animation6
+    }
+
+    let animation: Lottie
+    let loopMode: LottieLoopMode = .playOnce
+
+    init(animation: LottieAnimatingView.Lottie) {
+        self.animation = animation
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<LottieAnimatingView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animation.rawValue, subdirectory: "Resources")
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
